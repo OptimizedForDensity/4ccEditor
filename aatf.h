813bd11a7e56ac8b3cdc02a6e1090372885bf054
OptimizedForDensity <105244635+OptimizedForDensity@users.noreply.github.com>
2025-09-11 08:26:53 -0400

    Update aatf.h

--- aatf.h
+++ aatf.h
@@ -2,32 +2,40 @@
 //============================
 //Settings
 extern int manletBonus;
+extern int bronzeManletBonus;
 extern int silverManletBonus;
 extern int goldManletBonus;
+extern int bronzeGiantPen;
 extern int silverGiantPen;
 extern int goldGiantPen;
 
 extern int goldRate;
 extern int silverRate;
+extern int bronzeRate;
 extern int regRate;
 extern int gkRate;
 extern int reqNumGold;
 extern int reqNumSilver;
+extern int reqNumBronze;
 
 extern int goldForm;
 extern int silverForm;
+extern int bronzeForm;
 extern int regForm;
 
 extern int goldIR; //Injury resistence
 extern int silverIR;
+extern int bronzeIR;
 extern int regIR;
 
 extern int goldWeakFootUse;
 extern int silverWeakFootUse;
+extern int bronzeWeakFootUse;
 extern int regWeakFootUse;
 
 extern int goldWeakFootAcc;
 extern int silverWeakFootAcc;
+extern int bronzeWeakFootAcc;
 extern int regWeakFootAcc;
 
 extern int gkSkillCards;
@@ -37,10 +45,12 @@ extern int goldSkillCards;
 
 extern int gkTrickCards;
 extern int regTrickCards;
+extern int bronzeTrickCards;
 extern int silverTrickCards;
 extern int goldTrickCards;
 
 extern int regCOM;
+extern int bronzeCOM;
 extern int silverCOM;
 extern int goldCOM;
 
@@ -62,4 +72,5 @@ extern int heightTall;
 extern int heightTallGK;
 extern int heightMid;
 extern int heightManlet;
+
 
