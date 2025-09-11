74596a04cf9d4716078678cbb5dadc0d29b46bcd
OptimizedForDensity <105244635+OptimizedForDensity@users.noreply.github.com>
2025-09-11 08:12:42 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -177,7 +177,7 @@ int APIENTRY _tWinMain(HINSTANCE I, HINSTANCE PI, LPTSTR CL, int SC)
 	ghw_main = CreateWindowEx(
 		0,
 		wc.lpszClassName,
-		_T("4ccEditor Summer 25 Edition (Version B)"),
+		_T("4ccEditor Akko League VI Edition (Version A)"),
 		WS_OVERLAPPEDWINDOW,
 		20, 20, 1120+144, 700,
 		NULL, NULL, ghinst, NULL);
@@ -5810,6 +5810,7 @@ void update_tables()
 		}
 	}
 }
+
 
 
 
