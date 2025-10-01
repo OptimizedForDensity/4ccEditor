608a31fa1a1564b363f0f2df3b9e1ca577a9e780
OptimizedForDensity <105244635+OptimizedForDensity@users.noreply.github.com>
2025-10-01 16:56:45 -0400

    Bump version

--- main.cpp
+++ main.cpp
@@ -177,7 +177,7 @@ int APIENTRY _tWinMain(HINSTANCE I, HINSTANCE PI, LPTSTR CL, int SC)
 	ghw_main = CreateWindowEx(
 		0,
 		wc.lpszClassName,
-		_T("4ccEditor Akko League VI Edition (Version C)"),
+		_T("4ccEditor Akko League VI Edition (Version D)"),
 		WS_OVERLAPPEDWINDOW,
 		20, 20, 1120+144, 700,
 		NULL, NULL, ghinst, NULL);
